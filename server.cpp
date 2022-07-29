fff9df90691a262d75bd03e3bef2de02ec203156
Arthur <92685845+Artur420Art@users.noreply.github.com>
2022-07-29 19:47:46 +0400

    Update server.cpp
    fix bind error

--- server.cpp
+++ server.cpp
@@ -1,90 +1,97 @@
 #include <iostream>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
 #include <unistd.h>
-#define PORT 8080
+#include <netdb.h>
 
 int main() {
     int client, server;
-    int portnum = 1500;
+    int portNum = 1500;
     bool isExit = false;
-    int buffsize = 1024;
-    char buffer[buffsize];
+    int bufsize = 1024;
+    char buffer[bufsize];
 
     struct sockaddr_in server_addr;
     socklen_t size;
-    client = socket(AF_INET, SOCK_STREAM, 0);
+
+    client = socket(AF_INET6, SOCK_STREAM, 0);
 
     if (client < 0) {
-        std::cout << "Erorr connection";
-        exit(1);
+        std::cout<<"Error socket ..."<<std::endl;
+        exit(1); 
     }
-    std::cout << "socket server connection created..."<<std::endl;
 
-    server_addr.sin_family = AF_INET;
+    std::cout<<"socket is created"<<std::endl;
+
+    server_addr.sin_family = AF_INET6;
     server_addr.sin_addr.s_addr = htons(INADDR_ANY);
-    server_addr.sin_port = htons(portnum);
+    server_addr.sin_port = htons(portNum);
 
-    if ((bind(client,(struct sockaddr*) & server_addr, sizeof(server_addr))) < 0) {
-        std::cout<< "=> Error binding connection"<<std::endl;
+    if ((bind(client, (struct sockaddr*)&server_addr, sizeof(server_addr))) < 0) {
+        std::cout<<"Error binding connection"<<std::endl;
         return -1;
-    }
+    } 
 
     size = sizeof(server_addr);
-    std::cout<<"looking for clients..."<<std::endl;
+    std::cout<<"Looking clients"<<std::endl;
     listen(client, 1);
 
     int client_count = 1;
-    server = accept(client, (struct sockaddr* )&server_addr, &size);
-    if (server < 0)
-    {
-        std::cout<<"Error on accepting..."<<std::endl;
-    }
+    server = accept(client, (struct sockaddr*)&server_addr, &size);
 
-    while (server > 0) {
-        strcpy(buffer, "=> server connected...\n");
-        send(server, buffer, buffsize, 0);
-        std::cout << "=> Connected with the client #" << client_count << ", you are good to go..." <<std::endl;
-        std::cout << "\n=> Enter # to end the connection\n" << std::endl;
-    }
-    
-    std::cout<<"client: ";
-    do {
-        recv(server, buffer, buffsize, 0);
-        std::cout<<buffer<<" ";
-        if (*buffer == '#') {
-            *buffer = '*';
-            isExit = true;
+    if (server < 0) {
+        std::cout<<"Error on accepting"<<std::endl;
+    } 
 
-        }
-    }while(*buffer != '*');
+    while(server > 0) {
+        strcpy(buffer, "server connected...\n");
+        send(server, buffer, bufsize, 0);
+        std::cout<<"connecting with client "<<client_count <<std::endl;
+        std::cout<<"Enter to end connection"<<std::endl;
+
+        std::cout<<"client:";
+        do {
+            recv(server, buffer, bufsize, 0);
+            std::cout<<buffer<<" ";
+            if (*buffer == '\r') {
+                *buffer = '*';
+                isExit = true;
+
+            }
+        }while(*buffer != '*');
 
         do {
-           std::cout << "\nServer: ";
+            std::cout<<"\nServer: ";
             do {
-                std::cin >> buffer;
-                send(server, buffer, buffsize, 0);
-                if (*buffer == '#') {
-                    send(server, buffer, buffsize, 0);
+                std::cin>>buffer;
+                send(server, buffer, bufsize, 0);
+                if (*buffer =='\r') {
+                    send(server, buffer, bufsize, 0);
                     *buffer = '*';
                     isExit = true;
                 }
-            } while (*buffer != '*');
-
-            std::cout << "Client: ";
+            }while(*buffer != '*');
+            std::cout<<"Client: ";
             do {
-                recv(server, buffer, buffsize, 0);
-                std::cout << buffer << " ";
-                if (*buffer == '#') {
-                    *buffer == '*';
+                recv(server, buffer, bufsize, 0);
+                std::cout<< buffer << " ";
+                if (*buffer == '\r') {
+                    *buffer = '*';
                     isExit = true;
                 }
-            } while (*buffer != '*');
-        } while (!isExit);
+            }while(*buffer != '*');
+        }while(!isExit);
 
+        close(server);
+        std::cout<<"GOODBYE..";
+        isExit = false;
+        exit(1);
+    }
 
+    close(client);
     return 0;
 }
