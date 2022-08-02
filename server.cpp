fb641414152b08285b2acef5cb23991b970fcb3c
Arthur <92685845+Artur420Art@users.noreply.github.com>
2022-08-02 22:43:56 +0400

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,119 +1,87 @@
-
 #include <iostream>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <netdb.h>
+#include <string>
 #include <stdlib.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#define DEF_PORT 1601
+#define BUFFERSIZE 1024
+#define CLIENT_EXIT '#'
 
+bool is_client_connected(const char* msg);
+int main() {
+  int client, server;
+  struct sockaddr_in server_addres;
 
+  client = socket(AF_INET, SOCK_STREAM, 0);
+  if (client < 0) {
+    std::cout<< "Error socket not created";
+  }
+  std::cout<<"socket has been created"<<std::endl;
 
-int main()
-{
-    
-    int client, server;
-    int portNum = 1500;
-    bool isExit = false;
-    int bufsize = 1024;
-    char buffer[bufsize];
+  server_addres.sin_addr.s_addr = htons(INADDR_ANY);
+  server_addres.sin_family = AF_INET;
+  server_addres.sin_port = htons(DEF_PORT);
 
-    struct sockaddr_in server_addr;
-    socklen_t size;
+  int ret = bind(client, reinterpret_cast<struct sockaddr*>(&server_addres), sizeof(server_addres)); 
 
+  if (ret < 0) {
+    std::cout<< "Error bind faild connection"<<std::endl;
+    return -1;
+  }
+  socklen_t size = sizeof(server_addres);
+  std::cout<<"SERVER:"<<"Listening client"<<std::endl;
+  listen(client, 1);
 
-    client = socket(AF_INET, SOCK_STREAM, 0);
+  server = accept(client, reinterpret_cast<sockaddr*>(&server_addres), &size);
+  if (server < 0) {
+    std::cout<<"Error to connection";
+  }
+  char buffer[BUFFERSIZE];
+  bool is_exit = false;
+  while (server > 0)
+  {
+    strcpy(buffer, "server connected!\n");
+    send(server, buffer, BUFFERSIZE, 0);
+    std::cout<<"client connected"<<std::endl;
+    std::cout<<"Enter"<<CLIENT_EXIT<<"to disconect\n";
 
-    if (client < 0) 
-    {
-        std::cout << "\nError establishing socket..." << std::endl;
-        exit(1);
+    std::cout<<"Client:";
+    recv(server, buffer, BUFFERSIZE, 0);
+    std::cout<<buffer<<std::endl;
+
+    if (is_client_connected(buffer)) {
+      is_exit = true;
     }
-
-
-    std::cout << "\n=> Socket server has been created..." << std::endl;
-
-
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htons(INADDR_ANY);
-    server_addr.sin_port = htons(portNum);
-
-
-
-    if ((bind(client, (struct sockaddr*)&server_addr,sizeof(server_addr))) < 0) 
+  
+    while (!is_exit)
     {
-        std::cout << "=> Error binding connection, the socket has already been established..." << std::endl;
-        return -1;
+      std::cout<<"Server:";
+      std::cin.getline(buffer, BUFFERSIZE);
+      send(server, buffer, BUFFERSIZE, 0);
+      if (is_client_connected(buffer)) {
+        break;
+      }
+      std::cout<<"Client: ";
+      recv(server, buffer, BUFFERSIZE, 0);
+      std::cout<<buffer<<std::endl;
+      if (is_client_connected(buffer)) {
+        break;
+      }
     }
-
-
-    size = sizeof(server_addr);
-    std::cout << "=> Looking for clients..." << std::endl;
-
-
-    listen(client, 1);
-
-
-    int clientCount = 1;
-    server = accept(client,(struct sockaddr *)&server_addr,&size);
-
-
-    if (server < 0) 
-        std::cout << "=> Error on accepting..." << std::endl;
-
-    while (server > 0) 
-    {
-        strcpy(buffer, "=> Server connected...\n");
-        send(server, buffer, bufsize, 0);
-        std::cout << "=> Connected with the client #" << clientCount << ", you are good to go..." << std::endl;
-        std::cout << "\n=> Enter # to end the connection\n" << std::endl;
-
-        
-
-        std::cout << "Client: ";
-        do {
-            recv(server, buffer, bufsize, 0);
-            std::cout << buffer << " ";
-            if (*buffer == '#') {
-                *buffer = '*';
-                isExit = true;
-            }
-        } while (*buffer != '*');    
-
-        do {
-            std::cout << "\nServer: ";
-            do {
-                std::cin >> buffer;
-                send(server, buffer, bufsize, 0);
-                if (*buffer == '#') {
-                    send(server, buffer, bufsize, 0);
-                    *buffer = '*';
-                    isExit = true;
-                }
-            } while (*buffer != '*');
-
-            std::cout << "Client: ";
-            do {
-                recv(server, buffer, bufsize, 0);
-                std::cout << buffer << " ";
-                if (*buffer == '#') {
-                    *buffer = '*';
-                    isExit = true;
-                }
-            } while (*buffer != '*');
-        } while (!isExit);
-
-
-
-        std::cout << "\n\n=> Connection terminated with IP " << inet_ntoa(server_addr.sin_addr);
-        close(server);
-        std::cout << "\nGoodbye..." << std::endl;
-        isExit = false;
-        exit(1);
+      std::cout<<"\n Disconnected"<<std::endl<<"good bye";
+      is_exit = false;
+      exit(1);
     }
+}
 
-    close(client);
-    return 0;
+bool is_client_connected(const char* msg) {
+  for(int i = 0; i < strlen(msg); ++i) {
+      if(msg[i] == CLIENT_EXIT) {
+        return true;
+      }
+  }
+  return false;
 }
